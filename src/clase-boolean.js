02c087689671e94384c56e8a0dcf172dfe0c2bc3
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-07 17:11:08 -0300

    Clase 13 Curso de JavaScript desde cero

---
+++ src/clase-boolean.js
@@ -0,0 +1,21 @@
+const isActive = true
+const hasPermission = false
+
+//Conversión implicita
+const result = 5 > 3 
+
+console.log(result)
+
+const name = 'Platzi'
+
+console.log(!!name)
+
+//Conversión explícita
+const value = 0
+const otherValue = 41
+const explicitBoolean = Boolean(value)
+const explicitBoolean2 = Boolean(otherValue)
+
+console.log(explicitBoolean)
+console.log(explicitBoolean2)
+
