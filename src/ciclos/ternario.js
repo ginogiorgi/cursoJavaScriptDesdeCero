c79298b7d8fe633443947e1cbf64a762965886b7
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 15:55:26 -0300

    Clase 36 Curso de JavaScript desde cero

---
+++ src/ciclos/ternario.js
@@ -0,0 +1,14 @@
+// let mensaje
+// const edad= 20
+
+// if (edad >= 18) {
+//     mensaje = "Es mayor de edad, puede pasar"
+// } else {
+//     mensaje = "Lo siento, no es mayor de edad, no puede pasar"
+// }
+// console.log(mensaje)
+
+const edad = 20
+const mensaje = edad >= 18 ? "Es mayor de edad, puede pasar" : "Lo siento, es menor de edad, no puede pasar!"
+
+console.log(mensaje)
