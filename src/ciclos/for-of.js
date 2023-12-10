e506a144719ab14ea571e296927ade03cdcfdef9
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 16:22:36 -0300

    Clase 40 Curso de JavaScript desde cero

---
+++ src/ciclos/for-of.js
@@ -0,0 +1,11 @@
+// for of arrays, strings [algo]
+
+// for (variable of objeto) {
+//     código
+// }
+
+let frutas = ['Manzana', 'Pera', 'Naranja', 'Uva']
+
+for (fruta of frutas) {
+    console.log(fruta)
+}
