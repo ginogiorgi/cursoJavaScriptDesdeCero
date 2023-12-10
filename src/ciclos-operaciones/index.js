3c872080915bf9345f498cfe5447cc362d5d6cc3
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 13:51:14 -0300

    Clase 32 Curso de JavaScript desde cero

---
+++ src/ciclos-operaciones/index.js
@@ -0,0 +1,23 @@
+// Operadores de comparación
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+const a = 10
+const b = 20
+const c = '10'
+
+console.log(a == b)
+console.log(a != b)
+console.log(a == c)
+console.log(a === c)
+console.log(a > b)
+console.log(a < b)
+
+
