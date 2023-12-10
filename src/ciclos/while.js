ed54c93022442da8556a2bfe31a3d16960ffb048
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 16:33:29 -0300

    Clase 42 Curso de JavaScript desde cero

---
+++ src/ciclos/while.js
@@ -0,0 +1,11 @@
+// while(condición) {
+//     código
+// }
+// Mientras la condicion sea verdad el while se va a ejecutar
+
+let contador = 10
+
+while (contador >= 1) {
+    console.log(`Te explota el ano en ${contador}`)
+    contador --
+}
