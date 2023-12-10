b5795afaf5d6263bbc7460bb3013708da59f4251
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 16:37:40 -0300

    Clase 43 Curso de JavaScript desde cero

---
+++ src/ciclos/do-while.js
@@ -0,0 +1,12 @@
+// do {
+//     código
+// } while (condición)
+
+// Priemro se ejecuta el codigo y despues se chekea la condición
+
+let contador = 0
+
+do {
+    console.log(`El contador va en ${contador}`)
+    contador++
+} while (contador < 10)
