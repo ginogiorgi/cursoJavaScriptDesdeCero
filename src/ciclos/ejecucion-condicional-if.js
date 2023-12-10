1ede316ce74ef5ce4982314b02e2487f94205b5f
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 15:10:02 -0300

    Clase 34 Curso de JavaScript desde cero

---
+++ src/ciclos/ejecucion-condicional-if.js
@@ -0,0 +1,17 @@
+// if (let === algo) {
+//     //código
+// } else if (let === otraCosa){
+//     //código
+// } else {
+//     //código
+// }
+
+let nombre = 'Diego'
+
+if (nombre === 'Diego') {
+    console.log('Hola Diego')
+} else if (nombre === 'Nico') {
+    
+} else {
+    console.log('Nombre no encontrado')
+}
