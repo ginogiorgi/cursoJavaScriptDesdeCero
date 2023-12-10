33311cd960d70f488dc7eaf3973ad2bc6845e1c4
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-10 16:14:34 -0300

    Clase 38 Curso de JavaScript desde cero

---
+++ src/ciclos/for.js
@@ -0,0 +1,9 @@
+// for (variable; condición; incremento) {
+//     //código 
+// }
+
+let list = ['eat', 'sleep', 'code', 'repeat']
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+}
